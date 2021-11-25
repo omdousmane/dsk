58cd850bf3fe2a716c2b1f9d2b06b6c3e59de5ba
ousmane <omdousmane@gmail.com>
2021-11-25 13:37:52 +0100

    firtscommit

---
+++ admin/design/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", () => {
+  (function scroll() {
+    const header = document.getElementById("autofixed");
+    if (header) {
+      if (window.pageYOffset > 400) {
+        header.classList.add("header-animated");
+      } else {
+        header.classList.remove("header-animated");
+      }
+      if (window.pageYOffset > 1200) {
+        header.classList.add("fixed-top");
+      } else {
+        header.classList.remove("fixed-top");
+      }
+    }
+  })();
+});
---
+++ pages/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include './includes/head.php'; ?>
+    <title>Inscription</title>
+</head>
+
+<body>
+    <div class="">
+        <form action="#" class="contact">
+            <h3>Inscription</h3>
+            <input type="text" class="btn-input mb-10" placeholder="Nom">
+            <input type="text" class="btn-input mb-10" placeholder="Email">
+            <input type="button" value="Envoyer" class="btn">
+        </form>
+    </div>
+</body>
+
+</html>
